905cd144edc8c65bca982f8ea2731ee8852b3276
TheTrueYak <107325508+TheTrueYak@users.noreply.github.com>
2025-07-06 05:19:18 -0500

    small tweaks
    -version specification
    -removal of logger
    -apparently this shit works on old ass versions so that's cool

--- src/main/java/net/yak/lenientsuggestions/LenientSuggestions.java
+++ src/main/java/net/yak/lenientsuggestions/LenientSuggestions.java
@@ -2,15 +2,10 @@ package net.yak.lenientsuggestions;
 
 import net.fabricmc.api.ModInitializer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class LenientSuggestions implements ModInitializer {
 	public static final String MOD_ID = "lenientsuggestions";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("hey, mom made pancakes");
 	}
 }
